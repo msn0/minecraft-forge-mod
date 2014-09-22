37f9b95be977b53247cff40622300fd486d09768
Michał Jezierski <ambinanitelo@gmail.com>
2014-09-22 21:48:11 +0200

    hydrogen item

---
+++ src/main/java/tutorial/minecraft/HydrogenAtomItem.java
@@ -0,0 +1,13 @@
+package tutorial.minecraft;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class HydrogenAtomItem extends Item {
+    public HydrogenAtomItem(int par1){
+        super(par1);
+        setTextureName("examplemod:HydrogenAtom");
+        setUnlocalizedName("Hydrogen Atom");
+        }
+        
+    }
