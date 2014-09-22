a85b4001e695f1c0dd9efb750a65318dc703f92c
Michał Jezierski <ambinanitelo@gmail.com>
2014-09-22 21:47:42 +0200

    hand crusheritem

---
+++ src/main/java/tutorial/minecraft/HandCrusherItem.java
@@ -0,0 +1,13 @@
+package tutorial.minecraft;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class HandCrusherItem extends Item {
+    public HandCrusherItem(int par1){
+        super(par1);
+        setTextureName("examplemod:HandCrusher");
+        setUnlocalizedName("Hand Crusher");
+        }
+        
+    }
