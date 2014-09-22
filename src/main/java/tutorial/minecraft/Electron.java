3574edffe424f7cd28f562f284e6f2f5fbb79e99
Michał Jezierski <ambinanitelo@gmail.com>
2014-09-22 21:47:21 +0200

    electron item

---
+++ src/main/java/tutorial/minecraft/Electron.java
@@ -0,0 +1,12 @@
+package tutorial.minecraft;
+
+import net.minecraft.item.Item;
+
+public class Electron extends Item {
+    public Electron(int par1) {
+        super(par1);
+        setTextureName("examplemod:Electron");
+        setUnlocalizedName("Electron");
+    }
+
+}
