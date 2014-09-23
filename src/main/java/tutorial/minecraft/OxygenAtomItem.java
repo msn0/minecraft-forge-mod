3fd38de61057eb3ff0b2beeff61de3c3ee87f830
Michał Jezierski <ambinanitelo@gmail.com>
2014-09-23 07:29:57 +0200

    oxygen atom item

---
+++ src/main/java/tutorial/minecraft/OxygenAtomItem.java
@@ -0,0 +1,13 @@
+package tutorial.minecraft;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class OxygenAtomItem extends Item {
+    public OxygenAtomItem(int par1){
+        super(par1);
+        setTextureName("examplemod:OxygenAtom");
+        setUnlocalizedName("Oxygen Atom");
+        }
+        
+    }
