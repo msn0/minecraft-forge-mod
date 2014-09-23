e7a86ed7f65d0c7f2bc5f422ec256edc6f549fe8
Michał Jezierski <ambinanitelo@gmail.com>
2014-09-23 07:30:21 +0200

    proton item

---
+++ src/main/java/tutorial/minecraft/ProtonItem.java
@@ -0,0 +1,13 @@
+package tutorial.minecraft;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class ProtonItem extends Item {
+    public ProtonItem(int par1){
+        super(par1);
+        setTextureName("examplemod:Proton");
+        setUnlocalizedName("Proton");
+        }
+        
+    }
