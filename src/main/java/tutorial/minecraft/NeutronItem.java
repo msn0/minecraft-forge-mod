5ff587fb5d26221287880ccbddaa52b288e5d240
Michał Jezierski <ambinanitelo@gmail.com>
2014-09-23 07:29:17 +0200

    neutron item

---
+++ src/main/java/tutorial/minecraft/NeutronItem.java
@@ -0,0 +1,13 @@
+package tutorial.minecraft;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class NeutronItem extends Item {
+    public NeutronItem(int par1){
+        super(par1);
+        setTextureName("examplemod:Neutron");
+        setUnlocalizedName("Neutron");
+        }
+        
+    }
