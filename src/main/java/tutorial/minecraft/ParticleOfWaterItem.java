2f3a932fadd1a5ba4373988dd47dc77d0837158e
Michał Jezierski <ambinanitelo@gmail.com>
2014-09-23 07:30:57 +0200

    particle of water

---
+++ src/main/java/tutorial/minecraft/ParticleOfWaterItem.java
@@ -0,0 +1,13 @@
+package tutorial.minecraft;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class ParticleOfWaterItem extends Item {
+    public ParticleOfWaterItem(int par1){
+        super(par1);
+        setTextureName("examplemod:ParticleOfWater");
+        setUnlocalizedName("Particle Of Water");
+        }
+        
+    }
